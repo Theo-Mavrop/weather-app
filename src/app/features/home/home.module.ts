5d9688c5ea4ded683c1d0cf45c3556f0fba023ff
Theofylaktos Mavropoulos <th.mavrop@gmail.com>
2020-11-19 00:37:51 +0200

    Added search bar component to home page

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomeComponent } from './pages/home.component';
 import { HomeRoutingModule } from './home-routing.module';
-
-
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SearchBarComponent
   ],
   imports: [
     CommonModule,
